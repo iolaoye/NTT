e694aa12966b3b99e2e797ff0f74c1892a1a0481
brandonjurado <brandonjurado13@yahoo.com>
2016-04-04 15:06:01 -0500

    App. helpers now in SessionsHelper; Protect from CSRF attacks

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,26 @@
 module SessionsHelper
+
+  	def sign_in(user)
+		self.current_user = user
+	end
+
+	def sign_out
+		self.current_user = nil
+	end
+
+	def signed_in?
+		!current_user.nil?
+	end
+
+	def current_user=(user)
+		@current_user = user
+	end
+
+	def current_user
+		@current_user ||= User.find_by_id(session[:user_id])
+	end
+	
+	def current_user?(user)
+		user = current_user
+	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20160322133330) do
+ActiveRecord::Schema.define(:version => 20160404194909) do
 
   create_table "activities", :force => true do |t|
     t.string   "name"
