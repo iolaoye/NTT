3869c535be867e8af420881438b6900effefc1f9
oscargallegom <ogallego@tiaernet.tarleton.edu>
2016-04-05 11:46:18 -0500

    All 04/05/2016

--- app/models/field.rb
+++ app/models/field.rb
@@ -1,8 +1,8 @@
 class Field < ActiveRecord::Base
   attr_accessible :field_area, :field_average_slope, :field_name, :field_type, :location_id
   #Associations
-    has_one :weather, :dependent => :delete
+    has_one :weather, :dependent => :destroy
 	belongs_to :location
-	has_many :soils, :dependent => :delete_all
-	has_many :scenarios, :dependent => :delete_all
+	has_many :soils, :dependent => :destroy
+	has_many :scenarios, :dependent => :destroy
 end
--- app/models/soil.rb
+++ app/models/soil.rb
@@ -2,7 +2,7 @@ class Soil < ActiveRecord::Base
   attr_accessible :albedo, :drainage_type, :field_id, :group, :key, :name, :percentage, :selected, :slope, :symbol
   #associations
      belongs_to :field
-	 has_many :layers, :dependent => :delete_all
+	 has_many :layers, :dependent => :destroy
   #scopes
     default_scope :order => "percentage DESC"
 end
