a8fd733250b74ca5fbfb1071f98a19e1c3caabc2
Santi Gallego <santigallego9@gmail.com>
2016-06-03 14:27:23 -0500

    Santi 6/3/2016 db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20160531134358) do
+ActiveRecord::Schema.define(:version => 20160603151501) do
 
   create_table "activities", :force => true do |t|
     t.string   "name"
@@ -315,6 +315,7 @@ ActiveRecord::Schema.define(:version => 20160531134358) do
     t.datetime "updated_at",  :null => false
     t.integer  "scenario_id"
     t.integer  "subtype_id"
+    t.float    "moisture"
   end
 
   create_table "parameters", :force => true do |t|
@@ -327,6 +328,14 @@ ActiveRecord::Schema.define(:version => 20160531134358) do
     t.float    "range_low"
     t.float    "range_high"
     t.float    "default_value"
+  end
+
+  create_table "people", :force => true do |t|
+    t.string   "name"
+    t.integer  "age"
+    t.string   "last_name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "projects", :force => true do |t|
