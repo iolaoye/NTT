5a749b628c61ca13aaa022d7bd576b4525ee4fbd
oscargallegom <ogallego@tiaernet.tarleton.edu>
2016-07-06 16:28:50 -0500

    today

--- app/controllers/apex_layers_controller.rb
+++ app/controllers/apex_layers_controller.rb
@@ -3,55 +3,55 @@
   # GET /layers
   # GET /layers.json
   def index
-    @layers = Layer.where(:soil_id => params[:id])
+    @apex_layers = Layer.where(:soil_id => params[:id])
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @layers }
+      format.json { render json: @apex_layers }
     end
   end
 
   # GET /layers/1
   # GET /layers/1.json
   def show
-    @layer_id = params[:id]
-    @layer = Layer.find(params[:id])
+    @apex_layer_id = params[:id]
+    @apex_layer = Layer.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @layer }
+      format.json { render json: @apex_layer }
     end
   end
 
   # GET /layers/new
   # GET /layers/new.json
   def new
-    @layer = Layer.new
+    @apex_layer = Layer.new
 
     respond_to do |format|
       format.html # new.html.erb
-      format.json { render json: @layer }
+      format.json { render json: @apex_layer }
     end
   end
 
   # GET /layers/1/edit
   def edit
-    @layer_id = params[:id]
+    @apex_layer_id = params[:id]
     @apex_layer = Layer.find(params[:id])
   end
 
   # POST /layers
   # POST /layers.json
   def create
-    @layer = Layer.new(layer_params)
+    @apex_layer = Layer.new(layer_params)
 
     respond_to do |format|
-      if @layer.save
-        format.html { redirect_to @layer, notice: 'Layer was successfully created.' }
-        format.json { render json: @layer, status: :created, location: @layer }
+      if @apex_layer.save
+        format.html { redirect_to @apex_layer, notice: 'Layer was successfully created.' }
+        format.json { render json: @apex_layer, status: :created, location: @apex_layer }
       else
         format.html { render action: "new" }
-        format.json { render json: @layer.errors, status: :unprocessable_entity }
+        format.json { render json: @apex_layer.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -59,15 +59,14 @@
   # PATCH/PUT /layers/1
   # PATCH/PUT /layers/1.json
   def update
-    @layer = Layer.find(params[:id])
-
+    @apex_layer = Layer.find(params[:id])
     respond_to do |format|
-      if @layer.update_attributes(layer_params)
-        format.html { redirect_to list_layer_path(@layer.soil_id), notice: 'Layer was successfully updated.' }
+      if @apex_layer.update_attributes(layer_params)
+        format.html { redirect_to apex_layers_path, notice: 'Layer was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
-        format.json { render json: @layer.errors, status: :unprocessable_entity }
+        format.json { render json: @apex_layer.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -75,11 +74,11 @@
   # DELETE /layers/1
   # DELETE /layers/1.json
   def destroy
-    @layer = Layer.find(params[:id])
-    @layer.destroy
+    @apex_layer = Layer.find(params[:id])
+    @apex_layer.destroy
 
     respond_to do |format|
-      format.html { redirect_to list_layer_path(@layer.soil_id), notice: 'Layer was successfully updated.' }
+      format.html { redirect_to list_layer_path(@apex_layer.soil_id), notice: 'Layer was successfully updated.' }
       format.json { head :no_content }
     end
   end
