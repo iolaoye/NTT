8cd606322f3d0345b810e6c866e63bebf51bad0c
Santi Gallego <santigallego9@gmail.com>
2016-07-19 11:41:01 -0500

    fixed watershed deleting issue

--- app/models/watershed.rb
+++ app/models/watershed.rb
@@ -1,7 +1,7 @@
 class Watershed < ActiveRecord::Base
   attr_accessible :field_id, :name, :scenario_id
   #associations
-	has_many :watersheds, :dependent => :destroy
+	  has_many :watershed_scenarios, :dependent => :destroy
     belongs_to :location
   #validations
     validates_uniqueness_of :name
