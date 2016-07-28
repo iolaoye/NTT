d08dbd67624f6720f9a5ddf6865379c556a8091a
Santi Gallego <santigallego9@gmail.com>
2016-07-28 15:56:33 -0500

    added defualt value of 0.0 if nil to all fields

--- app/models/layer.rb
+++ app/models/layer.rb
@@ -4,5 +4,32 @@ class Layer < ActiveRecord::Base
     belongs_to :soil
   #scopes
     default_scope :order => "depth ASC"
-
+  #Intialization
+    after_initialize :init
+  #Functions
+  def init
+    #will set the default value to 0.0 only if it's nil
+    self.bulk_density  ||= 0.0
+    self.clay  ||= 0.0
+    self.depth  ||= 0.0
+    self.organic_matter  ||= 0.0
+    self.ph  ||= 0.0
+    self.sand  ||= 0.0
+    self.silt  ||= 0.0
+    #self.soil_id  ||= 0.0     #WTF: soil_id should never be nil, this should never execute
+    self.soil_p  ||= 0.0
+    self.uw  ||= 0.0
+    self.fc  ||= 0.0
+    self.wn  ||= 0.0
+    self.smb  ||= 0.0
+    self.woc  ||= 0.0
+    self.cac  ||= 0.0
+    self.cec  ||= 0.0
+    self.rok ||= 0.0
+    self.cnds  ||= 0.0
+    self.rsd  ||= 0.0
+    self.bdd  ||= 0.0
+    self.psp  ||= 0.0
+    self.satc  ||= 0.0
+  end
 end
