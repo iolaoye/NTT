ed9c8c4aca7fc2c6788ec05556814e6321f745b8
oscargallegom <ogallego@tiaernet.tarleton.edu>
2016-08-10 14:26:01 -0500

    uniqueness of projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,6 +6,6 @@ class Project < ActiveRecord::Base
 	has_many :apex_controls, :dependent => :destroy
 	has_many :apex_parameters, :dependent => :destroy
   #validations
-	 validates_uniqueness_of :name
+	 validates_uniqueness_of :name, :scope => user_id
 	 validates_presence_of :name
 end
