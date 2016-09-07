1fba75db4f94979b9ad50a33f4a3c2446bfa499b
Santi Gallego <santigallego9@gmail.com>
2016-09-07 14:38:21 -0500

    attempted fix

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -404,7 +404,7 @@ class ProjectsController < ApplicationController
       xml.subareas {
         subareas.each do |subarea|
           save_subarea_information(xml, subarea)
-        end # end 
+        end # end
       } # end xml.subareas
 
       charts = Chart.where(:soil_id => soil.id)
@@ -1311,7 +1311,7 @@ class ProjectsController < ApplicationController
               return msg
             end
           end
-      end # case end 
+      end # case end
     end # each element end
     if soil.save
       return "OK"
@@ -1975,7 +1975,7 @@ class ProjectsController < ApplicationController
           if soil_operation.activity_id == 4 then
             soil_operation.apex_operation = Crop.find_by_number_and_state(soil_operation.apex_crop, Location.find(session[:location_id].id).state_id).harvest_code
 			if soil_operation.apex_operation == nil then
-				soil_operation.apex_operation = Crop.find_by_number_and_state(soil_operation.apex_crop, "**").harvest_code				
+				soil_operation.apex_operation = Crop.find_by_number_and_state(soil_operation.apex_crop, "**").harvest_code
 			end
           end
       end
@@ -3155,5 +3155,5 @@ class ProjectsController < ApplicationController
       end # end case
     end # end each element
   end
-=end 
+=end
 end
