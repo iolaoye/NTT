59879d5fc1502609ad8b6bc41939a151cca5101c
oscargallegom <ogallego@tiaernet.tarleton.edu>
2016-11-06 18:50:33 -0600

    special projects. Location

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -2,7 +2,9 @@ class StatesController < ApplicationController
   # GET /states
   # GET /states.json
   def index
+
     @states = State.all
+	
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @states }
