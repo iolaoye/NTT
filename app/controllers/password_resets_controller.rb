3e86b4921dddbd2242fb213639ce35bcf24a2e09
Brandon Jurado <brandonjurado13@yahoo.com>
2017-06-21 14:07:13 -0500

    Operation changed to work with current -v rails

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,6 +1,6 @@
 class PasswordResetsController < ApplicationController
-  before_action :get_user, only: [:edit, :update]
-  before_action :check_expiration, only: [:edit, :update]
+  before_filter :get_user, only: [:edit, :update]
+  before_filter :check_expiration, only: [:edit, :update]
   def new
   end
 
