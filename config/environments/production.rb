069d3203c494f95602de42cd37cf74fd018e0db1
Brandon Jurado <brandonjurado13@yahoo.com>
2017-07-18 11:59:19 -0500

    Settings for production (test) server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ NTTG3::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_files = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -81,6 +81,8 @@ NTTG3::Application.configure do
     :enable_starttls_auto => true  }
 
   config.eager_load = true
+
+  config.log_level = :info
 end
 
 
