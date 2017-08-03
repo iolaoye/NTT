c7169754fedd96856876090da0d4812c769d32fd
Brandon Jurado <brandonjurado13@yahoo.com>
2017-08-03 07:03:26 -0700

    Fix for ActiveRecord access to Schedule

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,4 +1,4 @@
 class Schedule < ActiveRecord::Base
-  attr_accessible :activity_id, :apex_crop, :apex_fertilizer, :apex_operation, :apex_opv1, :apex_opv2, :crop_schedule_id, :day, :event_order, :self_id, :month, :year
-  self.primary_key = "self_id"
+  attr_accessible :activity_id, :apex_crop, :apex_fertilizer, :apex_operation, :apex_opv1, :apex_opv2, :crop_schedule_id, :day, :event_order, :id, :month, :year
+  self.primary_key = "id"
 end
