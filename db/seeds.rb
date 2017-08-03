fbf80d647b4ad0afe2f3a4f1f7a4db46dc4ff882
Brandon Jurado <brandonjurado13@yahoo.com>
2017-08-03 07:06:19 -0700

    All seeds needed for initial working project

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,24 @@
 # encoding: utf-8
 
+cont = true
+
+if cont then
+
+file = "db/seeds/schedules.rb"
+load file
+
+file = "db/seeds/irrigations.rb"
+load file
+
+file = "db/seeds/tillages.rb"
+load file
+
+file = "db/seeds/drainages.rb"
+load file
+
+file = "db/seeds/activities.rb"
+load file
+
 file = "db/seeds/descriptions.rb"
 load file
 
@@ -9,12 +28,47 @@ load file
 file = "db/seeds/control_descriptions.rb"
 load file
 
-cont = true
-
-if cont then
-file = "db/seeds/activities.rb"
+file = "db/seeds/ways.rb"
 load file
 
+file = "db/seeds/parameter_descriptions.rb"
+load file
+
+file = "db/seeds/bmplists.rb"
+load file
+
+file = "db/seeds/bmpsublists.rb"
+load file
+
+file = "db/seeds/counties.rb"
+load file
+
+file = "db/seeds/states.rb"
+load file
+
+file = "db/seeds/cropping_systems.rb"
+load file
+
+file = "db/seeds/crop_schedules.rb"
+load file
+
+file = "db/seeds/crops.rb"
+load file
+
+file = "db/seeds/fertilizer_types.rb"
+load file
+
+file = "db/seeds/fertilizers.rb"
+load file
+
+file = "db/seeds/manure_controls.rb"
+load file
+
+end
+
+cont = false
+
+if cont then
 file = "db/seeds/animals.rb"
 load file
 
@@ -30,40 +84,13 @@ load file
 file = "db/seeds/bmps.rb"
 load file
 
-file = "db/seeds/bmplists.rb"
-load file
-
-file = "db/seeds/bmpsublists.rb"
-load file
-
 file = "db/seeds/charts.rb"
 load file
 
 file = "db/seeds/climates.rb"
 load file
 
-file = "db/seeds/counties.rb"
-load file
-
-file = "db/seeds/crops.rb"
-load file
-
-file = "db/seeds/crop_schedules.rb"
-load file
-
-file = "db/seeds/cropping_systems.rb"
-load file
-
-file = "db/seeds/drainages.rb"
-load file
-
 file = "db/seeds/events.rb"
-load file
-
-file = "db/seeds/fertilizers.rb"
-load file
-
-file = "db/seeds/fertilizer_types.rb"
 load file
 
 file = "db/seeds/fields.rb"
@@ -72,16 +99,10 @@ load file
 file = "db/seeds/grazing_parameters.rb"
 load file
 
-file = "db/seeds/irrigations.rb"
-load file
-
 file = "db/seeds/layers.rb"
 load file
 
 file = "db/seeds/locations.rb"
-load file
-
-file = "db/seeds/manure_controls.rb"
 load file
 
 file = "db/seeds/modifications.rb"
@@ -93,9 +114,6 @@ load file
 file = "db/seeds/parameters.rb"
 load file
 
-file = "db/seeds/parameter_descriptions.rb"
-load file
-
 file = "db/seeds/projects.rb"
 load file
 
@@ -105,9 +123,6 @@ load file
 file = "db/seeds/scenarios.rb"
 load file
 
-file = "db/seeds/schedules.rb"
-load file
-
 file = "db/seeds/sites.rb"
 load file
 
@@ -115,9 +130,6 @@ file = "db/seeds/soils.rb"
 load file
 
 file = "db/seeds/soil_operations.rb"
-load file
-
-file = "db/seeds/states.rb"
 load file
 
 file = "db/seeds/stations.rb"
@@ -139,9 +151,6 @@ file = "db/seeds/watersheds.rb"
 load file
 
 file = "db/seeds/watershed_scenarios.rb"
-load file
-
-file = "db/seeds/ways.rb"
 load file
 
 file = "db/seeds/weathers.rb"
