4d65db3d880d6988e133faf8d29c4295dbfe1a70
Kirstie Cook <kirstiea@gmail.com>
2017-10-03 15:06:46 -0500

    Changing coordinates from string to text

--- db/migrate/20170930010625_change_coordinates_format_in_field.rb
+++ db/migrate/20170930010625_change_coordinates_format_in_field.rb
@@ -1,5 +1,6 @@
 class ChangeCoordinatesFormatInField < ActiveRecord::Migration
   def change
     change_column :fields, :coordinates, :text 
+    change_column :locations, :coordinates, :text 
   end
 end
