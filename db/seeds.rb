a85365474783b6e3b785ce6a4953761af3e05456
Kirstie Cook <kirstiea@gmail.com>
2017-11-09 13:56:17 -0600

    adding crop_schedule to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,12 @@ load file
 file = "db/seeds/activities.rb"
 load file
 
+file = "db/seeds/crop_schedules.rb"
+load file
+
+file = "db/seeds/schedules.rb"
+load file
+
 cont = false
 
 if cont then
@@ -17,7 +23,6 @@ load file
 
 file = "db/seeds/animals.rb"
 load file
-
 
 file = "db/seeds/aplcat_parameters.rb"
 load file
