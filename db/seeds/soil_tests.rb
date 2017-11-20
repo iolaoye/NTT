9746bc954b0b89513bf510d2dcbe89d385be4cb4
GALLEGO <gallego@tsu100176-m-osx.local>
2017-11-20 15:01:45 -0600

    Adding Mehlich3 P to soil test

--- db/seeds/soil_tests.rb
+++ db/seeds/soil_tests.rb
@@ -2,3 +2,4 @@ SoilTest.delete_all
 SoilTest.create!({:id => 1, :name =>"Fe-strip P", :factor1 => 0.10, :factor2 => 1.61}, :without_protection => true)
 SoilTest.create!({:id => 2, :name =>"Olsen P", :factor1 => 14.8, :factor2 => 1.54}, :without_protection => true)
 SoilTest.create!({:id => 3, :name =>"Bray-1 P", :factor1 => 10.8, :factor2 => 0.99}, :without_protection => true)
+SoilTest.create!({:id => 4, :name =>"Mehlich3 P", :factor1 => 0, :factor2 => 1}, :without_protection => true)
