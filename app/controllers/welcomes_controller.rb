e6e95172a8dc528ae6dd4c3a7e84992af28f97ae
Brandon Jurado <brandonjurado13@yahoo.com>
2018-01-11 16:56:52 -0600

    New User form implemented into welcome screen

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -6,6 +6,7 @@ class WelcomesController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def index
@@ -13,6 +14,7 @@ class WelcomesController < ApplicationController
       @projects = Project.where(:user_id => session[:user_id])
       redirect_to user_projects_path(current_user)
   	else
+      session[:errors] = nil # clear new user error messages during sign in
      	redirect_to new_welcome_path
 	end
 	 
