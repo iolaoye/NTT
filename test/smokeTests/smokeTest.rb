d3d80646d1b6d74eb302e52b70d2d0dc2074aa02
preston <prestonsward@gmail.com>
2018-02-21 11:34:05 -0600

    beggining finished for create/delete project test

--- test/smokeTests/smokeTest.rb
+++ test/smokeTests/smokeTest.rb
@@ -10,9 +10,16 @@ driver.find_element(:id, "password").clear
 driver.find_element(:id, "password").send_keys "tSu123!!!"
 driver.find_element(:name, "commit").click
 
-#Create new project
+#Create and delete project
 driver.find_element(:id, "new_project").click
 driver.find_element(:id, "project_name").clear
 driver.find_element(:id, "project_name").send_keys "stest"
 driver.find_element(:id, "project_description").clear
 driver.find_element(:id, "project_description").send_keys "stest project description"
+driver.find_element(:name, "commit").click
+#driver.find_element(:class=>"col-sm-3 col-md-3").text.include? "stest"
+driver.find_element(:partial_link_text, 'Nutrient Tracking Tool').click
+driver.find_element(:partial_link_text, 'Last Modified').click
+driver.find_element(:partial_link_text, 'Last Modified').click
+driver.find_element(:id,"stest project description").click
+driver.switch_to.alert.accept()
