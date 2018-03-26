fe1d9ef74645957abf85257ed8c3495b3efa62e9
Shikhar Subedi <ckhar_subedi653@yahoo.com>
2018-03-26 13:46:43 -0500

    supplement parameter update

--- app/models/supplement_parameter.rb
+++ app/models/supplement_parameter.rb
@@ -1,5 +1,6 @@
 class SupplementParameter < ActiveRecord::Base
-  attr_accessor :dmi_rheifers
+  attr_accessor :dmi_rheifers, :starting_julian_day, :ending_julian_day, :for_dmi_cows, :for_dmi_bulls, :for_dmi_calves,
+  :for_dmi_heifers, :for_dmi_rheifers
   attr_accessible :code, :dmi_bulls, :dmi_calves, :dmi_code, :dmi_cows, :dmi_heifers, :green_water_footprint, :scenario_id
 
     #associations
