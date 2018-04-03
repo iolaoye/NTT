71d081de61304c28b52ab11252569a877077fe12
GALLEGO <gallego@tsu96950-m.tarleton.edu>
2018-04-02 21:04:29 -0500

    Changing simulation to t-nn again

--- config/initializers/global_parameters.rb
+++ config/initializers/global_parameters.rb
@@ -11,14 +11,14 @@
 	VERSION = "NTTG3"
 
 # URL to send information to NTT, which return the results
-	URL_TIAER = 'http://ntt.tarleton.edu/NTTRails/NNRestService.ashx' 
+	URL_TIAER = 'http://nn.tarleton.edu/NTTRails/NNRestService.ashx' 
 	URL_CLOUD = 'http://45.40.132.224/NTTRails/NNRestService.ashx' 
 
 # URL to get heat unitsß
 	URL_HU = 'http://nn.tarleton.edu/weather/service.asmx?WSDL'
 
 # URL to get weather information
-	URL_Weather = 'http://ntt.tarleton.edu/weather/service.asmx?WSDL'
+	URL_Weather = 'http://nn.tarleton.edu/weather/service.asmx?WSDL'
 
 # URL to get soils information
 	URL_SoilsInfo = 'http://ntt.tarleton.edu/GetSoils/NTTservice.asmx?WSDL'
