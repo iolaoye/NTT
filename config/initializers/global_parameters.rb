59c489739bee24750f229f8872626b54c2e29e57
Kirstie Cook <kirstiea@gmail.com>
2018-04-04 15:06:37 -0500

    simulation in nn 1

--- config/initializers/global_parameters.rb
+++ config/initializers/global_parameters.rb
@@ -11,7 +11,7 @@
 	VERSION = "NTTG3"
 
 # URL to send information to NTT, which return the results
-	URL_TIAER = 'http://ntt.tarleton.edu/NTTRails/NNRestService.ashx'
+	URL_TIAER = 'http://nn.tarleton.edu/NTTRails/NNRestService.ashx'
 	#URL_TIAER = 'http://nn.tarleton.edu/NTTRails/NNRestService.ashx'
 	URL_CLOUD = 'http://45.40.132.224/NTTRails/NNRestService.ashx'  
 
@@ -19,7 +19,7 @@
 	#URL_HU = 'http://nn.tarleton.edu/weather/service.asmx?WSDL'
 
 # URL to get weather information
-	URL_Weather = 'http://ntt.tarleton.edu/weather/service.asmx?WSDL'
+	URL_Weather = 'http://nn.tarleton.edu/weather/service.asmx?WSDL'
 	#URL_Weather = 'http://nn.tarleton.edu/weather/service.asmx?WSDL'
 
 # URL to get soils information
