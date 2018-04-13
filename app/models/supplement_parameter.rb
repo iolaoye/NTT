96ff0a0cb386b79c586cbc4ea1b76aefde3f0910
Shikhar Subedi <ckhar_subedi653@yahoo.com>
2018-04-13 14:42:40 -0500

    supplement parameter

--- app/models/supplement_parameter.rb
+++ app/models/supplement_parameter.rb
@@ -1,9 +1,9 @@
 class SupplementParameter < ActiveRecord::Base
-  attr_accessor :dmi_rheifers, :starting_julian_day, :ending_julian_day, :for_dmi_cows, :for_dmi_bulls, :for_dmi_calves,
-  :for_dmi_heifers, :for_dmi_rheifers
-  attr_accessible :code, :dmi_bulls, :dmi_calves, :dmi_code, :dmi_cows, :dmi_heifers, :green_water_footprint, :scenario_id
+  attr_accessible :code, :dmi_bulls, :dmi_calves, :dmi_code, :dmi_cows, :dmi_heifers, :ending_julian_day, :green_water_footprint, :green_water_footprint_supplement,
+      :starting_julian_day, :dmi_rheifers, :for_dmi_cows, :for_dmi_bulls, :for_dmi_heifers, :for_dmi_calves, :for_dmi_rheifers, :code_for, :for_button,
+      :supplement_button
 
-    #associations
+  #associations
 	  belongs_to :scenario
-
+  #validation
 end
