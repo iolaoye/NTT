a551026567a5279ecb15a0cac4b5016c78cc46cd
Kirstie Cook <kirstiea@gmail.com>
2018-07-11 16:56:31 -0500

    aplcat parameters

---
+++ db/migrate/20180616183314_add_column_to_aplcat_parameters.rb
@@ -0,0 +1,8 @@
+class AddColumnToAplcatParameters < ActiveRecord::Migration
+  def change
+    add_column :aplcat_parameters, :abwrh, :float
+    add_column :aplcat_parameters, :nocrh, :integer
+    add_column :aplcat_parameters, :abc, :integer
+    add_column :aplcat_parameters, :forage_id, :integer
+  end
+end
