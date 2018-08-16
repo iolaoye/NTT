683e22edf948ab5fa11b8a967802751ade9c7f85
Kirstie Cook <kirstiea@gmail.com>
2018-08-16 14:36:53 -0500

    debugger removed

--- app/helpers/scenarios_helper.rb
+++ app/helpers/scenarios_helper.rb
@@ -881,7 +881,7 @@ module ScenariosHelper
     #else
       #return res.body
     #end
-    debugger
+    
     client = Savon.client(wsdl: URL_SoilsInfo)
  	response = client.call(:send_soils, message: {"county" => County.find(@project.location.county_id).county_state_code, "state" => State.find(@project.location.state_id).state_name, "field_coor" => @field.coordinates.strip, "session" => session[:session_id]})
     if response.body[:send_soils_response][:send_soils_result] != "Error" then
