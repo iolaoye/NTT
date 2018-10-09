61892881d349d1ab260347483d889c07a0d1148a
Michael Osei <mosei27@live.com>
2018-10-09 15:22:09 -0500

    Add tables in erb for FEM

--- app/controllers/fem_controller.rb
+++ app/controllers/fem_controller.rb
@@ -1,4 +1,9 @@
 class FemController < ApplicationController
-  def fem_tables
-  end
+  def list
+    @feeds = FeedsAugmented.all
+    @equipment = MachineAugmented.all
+    @structure = FacilityAugmented.all
+    @other = FarmGeneral.all
+  end 
 end
+
---
+++ db/migrate/20181008201435_feeds_augmented.rb
@@ -0,0 +1,4 @@
+class FeedsAugmented < ActiveRecord::Migration
+  def change
+  end
+end
