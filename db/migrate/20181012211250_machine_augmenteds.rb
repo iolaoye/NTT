e3a7259e193e18e3f6afb85f24106683b94c2201
Michael Osei <mosei27@live.com>
2018-10-12 16:41:14 -0500

    Add value loading for fem

---
+++ db/migrate/20181012211250_machine_augmenteds.rb
@@ -0,0 +1,4 @@
+class MachineAugmenteds < ActiveRecord::Migration
+  def change
+  end
+end
---
+++ db/seeds/facility_augmenteds.rb
@@ -0,0 +1,24 @@
+require 'csv'
+
+csv_text = File.read(Rails.root.join('db', 'seeds', 'facilityAugmented.csv'))
+csv  = CSV.parse(csv_text, :headers => true)
+puts csv_text
+
+FacilityAugmented.delete_all
+csv.each do |row| 
+    puts row.to_hash
+    FacilityAugmented.create!(
+        {:name => row[1],
+         :lease_rate => row[2],
+         :new_price => row[3],
+         :current_price => row[4],
+         :life_remaining => row[5],
+         :maintenance_coeff => row[6],
+         :loan_interest_rate => row[7],
+         :interest_rate_equity => row[8],
+         :proportion_debt => row[9],
+         :year => row[10]
+        }, 
+        #:without_protection => true
+    )
+end
