6dfa76d00d9a2aa94dd9d4b1bbc37b3d1edc642a
GALLEGO <gallego@t-97490-osx.local>
2019-03-06 15:34:56 -0600

    Changing output folder for soils

--- app/helpers/scenarios_helper.rb
+++ app/helpers/scenarios_helper.rb
@@ -980,7 +980,7 @@ module ScenariosHelper
       #return res.body
     #end
     client = Savon.client(wsdl: URL_SoilsInfo)
- 	response = client.call(:send_soils, message: {"county" => County.find(@project.location.county_id).county_state_code, "state" => State.find(@project.location.state_id).state_name, "field_coor" => @field.coordinates.strip, "session" => session[:session_id]})
+ 	response = client.call(:send_soils, message: {"county" => County.find(@project.location.county_id).county_state_code, "state" => State.find(@project.location.state_id).state_name, "field_coor" => @field.coordinates.strip, "session" => session[:session_id], "outputFolder" => APEX_FOLDER + "/APEX" + session[:session_id]})
     if response.body[:send_soils_response][:send_soils_result] != "Error" then
       msg = "OK"
       msg = create_new_soils(YAML.load(response.body[:send_soils_response][:send_soils_result]))
