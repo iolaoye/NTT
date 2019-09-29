63bd7530c9807c64e9862b193a3eb675bbeba4be
Oscar Gallego <oscargallegom@hotmail.com>
2019-09-29 09:17:18 -0500

    Testin R script

--- lib/external_scripts/get_coords.r
+++ lib/external_scripts/get_coords.r
@@ -1,9 +1,10 @@
 args = commandArgs(trailingOnly = TRUE)
 require(rgdal)
-​
+​print(args)
 shfile <- readOGR(args[1], layer="NTT_Example")​
 polys = attr(shfile,'polygons')​
 npoly<-length(polys)​
+
 for (i in 1:npoly){​
 	ncoords<-length(shfile@polygons[[i]]@Polygons[[1]]@coords)/2
 	 cat(paste0(" Field: AOI",i,"|"))
