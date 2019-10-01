228df7b2310f8856a17c357cbac0ded18a21c21b
root <root@ip-172-31-46-23.us-west-2.compute.internal>
2019-10-01 20:33:03 +0000

    Updating get_coords from server

--- lib/external_scripts/get_coords.r
+++ lib/external_scripts/get_coords.r
@@ -1,18 +1,13 @@
 args = commandArgs(trailingOnly = TRUE)
 require(rgdal)
-#setwd("public/download/NTT_Example")
-#print("antes")
-#print(getwd())
-​s_f = readOGR(dsn=args[1], layer='NTT_Example')​
-#print(shfile)
-
-polys = attr(​s_f,'polygons')​
-npoly<-length(polys)​
-
-for (i in 1:npoly){​
-	ncoords<-length(​s_f@polygons[[i]]@Polygons[[1]]@coords)/2
-	 cat(paste0(" Field: AOI",i,"|"))
-	for(j in 1:ncoords) {
-  		cat(paste0(​s_f@polygons[[i]]@Polygons[[1]]@coords[j,1],",",​s_f@polygons[[i]]@Polygons[[1]]@coords[j,2])​," ")
-	}
+s_f = readOGR(dsn=args[1])
+polys = attr(s_f,'polygons')
+npoly<-length(polys)
+for (i in 1:npoly){
+    ncoords<-length(s_f@polygons[[i]]@Polygons[[1]]@coords)/2
+    cat(paste0(" Field: AOI",i,"|"))
+    for(j in 1:ncoords) {
+        cat(paste0(s_f@polygons[[i]]@Polygons[[1]]@coords[j,1],",",s_f@polygons[[i]]@Polygons[[1]]@coords[j,2]),"")
+    }
 }
+
