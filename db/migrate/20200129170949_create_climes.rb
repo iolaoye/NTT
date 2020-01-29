83f2eec2bead5ffcf0772481a4a26fe294a27cbc
GALLEGO <gallego@tsu97490-osx.tarleton.edu>
2020-01-29 11:23:38 -0600

    migrate clime2

--- db/migrate/20200129170949_create_climes.rb
+++ db/migrate/20200129170949_create_climes.rb
@@ -1,7 +1,7 @@
 class CreateClimes < ActiveRecord::Migration[5.2]
   def change
     create_table :climes do |t|
-      t.integer, :field_id
+      t.integer :field_id
       t.string :daily_weather
       t.timestamps
     end
