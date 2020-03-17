e160ffc1c911745d6ec75ac345e62a5cf5d3f532
Oscar Gallego <oscargallegom@hotmail.com>
2020-03-17 11:29:20 -0500

    Selecting all of the polygons1

--- lib/external_scripts/get_coords.r
+++ lib/external_scripts/get_coords.r
@@ -7,7 +7,6 @@ s_f_org = readOGR(dsn=args[1])
 s_f = spTransform(s_f_org, CRS("+proj=longlat +datum=WGS84"))
 polys = attr(s_f_org,'polygons')
 npoly<-length(polys)
-n = 1
 for (i in 1:npoly){
 	sub_polys = length(s_f@polygons[[i]]@Polygons)
 	for(l in 1:sub_polys) {
