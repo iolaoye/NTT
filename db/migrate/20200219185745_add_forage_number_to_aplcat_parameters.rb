013398929f1922c356cf944ab61806112d0f1466
Oscar Gallego <oscargallegom@hotmail.com>
2020-07-29 13:40:41 -0500

    fixing migrations of aplcat_parameters

--- db/migrate/20200219185745_add_forage_number_to_aplcat_parameters.rb
+++ db/migrate/20200219185745_add_forage_number_to_aplcat_parameters.rb
@@ -1,5 +1,7 @@
 class AddForageNumberToAplcatParameters < ActiveRecord::Migration[5.2]
   def change
-    add_column :aplcat_parameters, :number_of_forage, :string
+  	if !(ActiveRecord::Base.connection.column_exists?(:aplcat_parameters, :number_of_forage)) 
+	    add_column :aplcat_parameters, :number_of_forage, :string
+	end
   end
 end
