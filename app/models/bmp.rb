d0dd0dfc12408ace58115a207cc3bc7bf2608b28
ckhar653 <ckhar_subedi653@yahoo.com>
2020-09-17 11:50:26 -0500

    a value of 0 can now be saved in fertigation

--- app/models/bmp.rb
+++ app/models/bmp.rb
@@ -12,17 +12,17 @@ class Bmp < ActiveRecord::Base
 	  belongs_to :scenario
   #validations
     validates_uniqueness_of :bmpsublist_id, :scope => :scenario_id
-    validates :water_stress_factor, numericality: { greater_than: 0,  less_than_or_equal_to: 1 }, if: Proc.new {|bmp| bmp.bmpsublist_id == 1 || bmpsublist_id == 2} 
+    validates :water_stress_factor, numericality: { greater_than_or_equal_to: 0,  less_than_or_equal_to: 1 }, if: Proc.new {|bmp| bmp.bmpsublist_id == 1 || bmpsublist_id == 2} 
     validates :irrigation_efficiency, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 1 }, if: Proc.new {|bmp| bmp.bmpsublist_id == 1 || bmpsublist_id == 2 || bmpsublist_id == 9}
-    validates :irrigation_id, numericality: { greater_than: 0 }, if: Proc.new {|bmp| bmp.bmpsublist_id == 1 || bmpsublist_id == 2} 
-    validates :maximum_single_application, numericality: { greater_than: 0,  less_than: 100000 }, if: Proc.new {|bmp| bmp.bmpsublist_id == 1 || bmpsublist_id == 2} 
+    validates :irrigation_id, numericality: { greater_than: 0 }, if: Proc.new {|bmp| bmp.bmpsublist_id == 1 || bmpsublist_id == 2}
+    validates :maximum_single_application, numericality: { greater_than: 0,  less_than: 100000 }, if: Proc.new {|bmp| bmp.bmpsublist_id == 1 || bmpsublist_id == 2}
     validates :safety_factor, numericality: { greater_than_or_equal_to: 0,  less_than_or_equal_to: 100000 }, if: Proc.new {|bmp| bmp.irrigation_id == 7}
     # validates :depth, numericality: { greater_than: 0 }, if: Proc.new {|bmp| bmp.bmpsublist_id == 3}
     validates :area, numericality: { greater_than: 0,  less_than_or_equal_to: 100000 }, if: Proc.new {|bmp| bmp.bmpsublist_id == 6 || bmpsublist_id == 7 || bmpsublist_id == 8 || bmpsublist_id == 23}
     #validates :number_of_animals, numericality: { greater_than: 0 }, if: "bmpsublist_id == 10"
-    validates :days, numericality: { greater_than: 0,  less_than_or_equal_to: 365 }, if: Proc.new {|bmp| bmp.bmpsublist_id == 1 || bmpsublist_id == 2} 
+    validates :days, numericality: { greater_than: 0,  less_than_or_equal_to: 365 }, if: Proc.new {|bmp| bmp.bmpsublist_id == 1 || bmpsublist_id == 2}
     #validates :depth, numericality: { greater_than: 0 }, if: "bmpsublist_id == 10"
-    validates :width, numericality: { greater_than_or_equal_to: 0 }, if: Proc.new {|bmp| bmp.bmpsublist_id == 10} 
+    validates :width, numericality: { greater_than_or_equal_to: 0 }, if: Proc.new {|bmp| bmp.bmpsublist_id == 10}
     #validates :dry_manure, numericality: { greater_than: 0 }, if: "bmpsublist_id == 10"
     #validates :no3_n, numericality: { greater_than: 0, less_than: 1 }, if: "bmpsublist_id == 10"
     #validates :po4_p, numericality: { greater_than: 0, less_than: 1 }, if: "bmpsublist_id == 10"
@@ -34,12 +34,12 @@ class Bmp < ActiveRecord::Base
   	validates :org_p, numericality: { greater_than: 0, less_than: 100}, if: Proc.new {|bmp| bmp.bmpsublist_id == 18}
     validates :width, numericality: { greater_than: 0 }, if: Proc.new {|bmp| bmp.bmpsublist_id == 4 || bmpsublist_id == 5 || bmpsublist_id == 6 || bmpsublist_id == 7 || bmpsublist_id == 14 || bmpsublist_id == 15 || bmpsublist_id == 23}
     validates :width, numericality: { greater_than: 0 }, if: Proc.new {|bmp| bmp.bmpsublist_id == 13 && depth == 13}
-    validates :grass_field_portion, numericality: { greater_than_or_equal_to: 0 }, if: Proc.new {|bmp| bmp.bmpsublist_id == 12} 
+    validates :grass_field_portion, numericality: { greater_than_or_equal_to: 0 }, if: Proc.new {|bmp| bmp.bmpsublist_id == 12}
     #validates :grass_field_portion, numericality: { greater_than_or_equal_to: 0.25, less_than_or_equal_to: 0.75 }, if: "bmpsublist_id == 13 && depth == 12"
     #validates :buffer_slope_upland, numericality: { greater_than_or_equal_to: 0.25, less_than_or_equal_to: 1.0 }, if: "bmpsublist_id == 12 || bmpsublist_id == 13 || bmpsublist_id == 23"
-    validates :crop_width, numericality: { greater_than: 0 }, if: Proc.new {|bmp| bmp.bmpsublist_id == 15} 
-    validates :slope_reduction, numericality: { greater_than: 0,  less_than_or_equal_to: 100 }, if: Proc.new {|bmp| bmp.bmpsublist_id == 16} 
-    validates :sides, numericality: { greater_than: 0, less_than_or_equal_to: 4.0 }, if: Proc.new {|bmp| bmp.bmpsublist_id == 5 || bmpsublist_id == 5 || bmpsublist_id == 6 || bmpsublist_id == 7} 
+    validates :crop_width, numericality: { greater_than: 0 }, if: Proc.new {|bmp| bmp.bmpsublist_id == 15}
+    validates :slope_reduction, numericality: { greater_than: 0,  less_than_or_equal_to: 100 }, if: Proc.new {|bmp| bmp.bmpsublist_id == 16}
+    validates :sides, numericality: { greater_than: 0, less_than_or_equal_to: 4.0 }, if: Proc.new {|bmp| bmp.bmpsublist_id == 5 || bmpsublist_id == 5 || bmpsublist_id == 6 || bmpsublist_id == 7}
     #validates_uniqueness_of :bmp_id, :scope => :scenario_id, :message => "of this group already exists", if: "bmp_id == 1 || bmp_id == 8"
     #validates_uniqueness_of :bmp_id, :scope => :scenario_id, if: :pad_and_pipes_exists
 
