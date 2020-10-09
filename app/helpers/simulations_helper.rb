7b98a56a75a55a08624bc35d04e68ff1d383f860
Oscar Gallego <oscargallegom@hotmail.com>
2020-10-09 12:22:40 -0500

    Weithing results 4

--- app/helpers/simulations_helper.rb
+++ app/helpers/simulations_helper.rb
@@ -2109,7 +2109,7 @@ def send_file_to_DNDC(apex_string, file, state)
         one_result["orgp"] = tempa[58, 9].to_f * (KG_TO_LBS / HA_TO_AC)
         one_result["po4"] = tempa[76, 9].to_f * (KG_TO_LBS / HA_TO_AC)
         one_result["orgn"] = tempa[49, 9].to_f * (KG_TO_LBS / HA_TO_AC)
-        one_result["no3"] = tempa[67, 9].to_f * (KG_TO_LBS / HA_TO_AC)
+        #one_result["no3"] = tempa[67, 9].to_f * (KG_TO_LBS / HA_TO_AC)
         one_result["qn"] = tempa[245, 9].to_f * (KG_TO_LBS / HA_TO_AC)
         #tile drain averaged from all of the subareas instead of sub = 0 because it is not right.
         one_result["qdr"] = tempa[126, 9].to_f * MM_TO_IN
@@ -2162,6 +2162,7 @@ def send_file_to_DNDC(apex_string, file, state)
           pcp = 0
           biom = 0
           total_subs = 0
+          sub_surface_n_sum = 0
         else
           #if bmp != nil then    #if reservoir exists take the td and subsruface N values from soils instead of from edgo of the field (sub == 0)
           if session[:simulation] == "scenario" then     #if scenario the values are weithed. If watershed - todo.
@@ -2191,7 +2192,7 @@ def send_file_to_DNDC(apex_string, file, state)
 
         end  # end if sub == 0
         if subs == 0 then
-          one_result["no3"] += sub_surface_n_sum
+          one_result["no3"] = one_result["qn"] + sub_surface_n_sum
           results_data.push(one_result)
         end
       else
