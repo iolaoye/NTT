ae495bffa15b54c31934171951b57fd6aceebc1a
cbntt <cbntt@users.noreply.github.com>
2020-12-18 17:25:44 -0600

    Revert "Jennifer nov"

--- db/migrate/20201218203811_remove_columns_from_county_result.rb
+++
@@ -1,6 +0,0 @@
-class RemoveColumnsFromCountyResult < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :county_results, :crop_ids, :string
-    remove_column :county_results, :bmp_id, :integer
-  end
-end
